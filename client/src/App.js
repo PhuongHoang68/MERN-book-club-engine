dd8754500d7dc1bb878bf6d6640654f607045d66
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2022-12-16 22:49:38 -0600

    added saveBook and removeBook mutations in back end

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink} from "@apollo/client";
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+//establishing a new link to GraphQL server at graphql endpoint
+const httpLink = createHttpLink({
+  uri: "http://localhost:3001/graphql",
+});
+
+//creating new Apollo Client instance and creating the connection to the API endpoint. Also instantiating a new cache object to pass info more efficiently
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +30,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,6 @@ type User {
     _id: ID
     username: String
     email: String
-    password: String
     savedBooks: [Book]
 },
 type Book {
