bf7f4583bb075c48fd1c2cf1f5e491e1b23c7ad6
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2022-12-22 15:55:11 -0600

    fixed deletebooks

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -61,9 +61,9 @@ const resolvers = {
         if(context.user) {
           const updatedUser = await User.findOneAndUpdate(
             {_id: context.user._id},
-            {$pull: { savedBooks: {bookId: bookId}}},
+            {$pull: { savedBooks: {bookId}}},
             { new: true}
-          ).populate("savedBooks");
+          );
 
           return updatedUser
         }
