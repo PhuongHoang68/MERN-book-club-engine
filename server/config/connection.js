c518079d283cbd145af1e530deaa77816bbad7d8
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2022-12-22 14:23:10 -0600

    deploying databse

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookclub', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-lovers-united', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -10,3 +10,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookclub', {
 });
 
 module.exports = mongoose.connection;
+
+
